e0c0d3000845f1bcd3386af9d187c91977216658
Theodore John Josef <john.josef@aol.com>
2023-01-31 01:28:17 -0800

    commit tree 90f9a3d4eb3b29bc269daa35319cfc36b253c155

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,9 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show 
+    #byebug
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 end
